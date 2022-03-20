92d6f8b0ecb24cb928226e762e87d63ae4bf5701
2GJ <GermanGJ@Hotamil.com>
2022-03-19 23:17:15 -0500

    2GJ: Se instala strategi google oauth20 {45:24}

--- loginnodeserver/index.js
+++ loginnodeserver/index.js
@@ -1,10 +1,21 @@
 const cookieSession = require("cookie-session");
 const express = require("express");
+const passport = require("passport");
+const cors = require("cors");
 const app = express();
 
 app.use(
     cookieSession({name:"session", keys:["lama"], maxAge: 24 * 60 * 100})
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cors({
+    origin:"https://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credential: true, 
+}))
 
 app.listen("5000", () => {
     console.log("Server is running!!!!");
