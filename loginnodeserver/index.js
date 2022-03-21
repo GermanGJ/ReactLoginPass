9ba0b62ef1e42b5b81f350a2977b3c2d151c9508
2GJ <GermanGJ@Hotamil.com>
2022-03-21 10:37:02 -0500

    2GJ: Se esta terminando el login google {58:55}

--- loginnodeserver/index.js
+++ loginnodeserver/index.js
@@ -1,6 +1,8 @@
 const cookieSession = require("cookie-session");
 const express = require("express");
+const passportSetup = require("./passport");
 const passport = require("passport");
+const authRoute = require("./routes/auth");
 const cors = require("cors");
 const app = express();
 
@@ -15,7 +17,9 @@ app.use(cors({
     origin:"https://localhost:3000",
     methods: "GET,POST,PUT,DELETE",
     credential: true, 
-}))
+}));
+
+app.use("/auth", authRoute);
 
 app.listen("5000", () => {
     console.log("Server is running!!!!");
---
+++ loginnodeserver/passport.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const GOOGLE_CLIENT_ID = "200185081150-r5oc8qbcqhjqqvi87q5v482dlocs0fm9.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    done(null,profile);
+  }
+));
+
+passport.serializeUser((user,done)=>{
+    done(null,user)
+})
+
+passport.deserializeUser((user,done)=>{
+    done(null,user)
+})
