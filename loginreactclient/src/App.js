d0a2b7d8d24ea65160c82b7ec283faaa8849c231
German Joaquin Gomez Jimenez <German.Gomez@bizagi.com>
2022-07-08 23:52:49 -0500

    2GJ: Paso 1 Login google {1:05:00}

--- loginreactclient/src/App.js
+++ loginreactclient/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Home } from './pages/Home';
 import { Navbar } from "./components/Navbar";
@@ -10,7 +10,32 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
 
 function App() {
-  const user = true;
+  const [user, setUser] = useState(null);
+
+useEffect(() => {
+  const getUser = () => {
+    fetch("http://localhost:5000/auth/login/success", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true,
+      },
+    }).then(response=>{
+      if(response.status==200) return response.json();
+      throw new Error("authentication has been failed!")
+    }).then(resObject=>{
+      setUser(resObject.user)
+    }).catch(err=>{
+      console.log(err);
+    });
+  };
+  getUser();
+}, []);
+
+console.log(user);
+
   return (
     <BrowserRouter>
       <div>
